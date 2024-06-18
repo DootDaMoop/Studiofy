5b9ef60620d63e957e4ae101c2f1b932e6775b3d
LenoMai <lenamai4002@gmail.com>
2024-06-17 21:41:40 -0400

    draft

--- client/pages/about.js
+++ client/pages/about.js
@@ -4,31 +4,59 @@ import styles from "../styles/About.module.css"
 function about() {
   return (
     <div className={styles.container}>
-        <h1 className={styles.header}>About Us</h1>
-        <hr></hr>
-        <div>
-            <p className={styles.aboutText}>[ Name of our Project ] Creates a personalized Pixel Studio Apartment based on your Spotify Music Tastes. We analyze your top songs using the Spotify Web API and design a living Space that reflects your musical preferences.
-                <br></br><br></br>You will be able to save Your Studio Apartment and compare the next time you generate a new one with different music tastes! Additionally, you can share your Studio apartment for others to see.
+
+        <div className={styles.menuContainer}>
+            <img src='/images/tempMenu.png' alt='Temp Menu Button'></img>
+        </div>
+
+        <div className={styles.headerContainer}>
+            <h1 className={styles.header}>About Studiofy</h1>
+            <div className={styles.line}></div>
+        </div>
+
+        <div className={styles.aboutContainer}>
+            <p className={styles.aboutText}>Studiofy creates a isometric studio apartment through pixel art based on your Spotify Music Tastes. We analyze your top 50 songs using the Spotify Web API and design the apartment based off your results..
+                <br></br><br></br>You will be able to save the apartment and compare the next time you generate a new one with different music tastes! Additionally, you can share your Studio apartment for others to see.
             </p>
         </div>
 
-        <h1 className={styles.header}>Meet The Development Team</h1>
-        <hr></hr>
+        <div className={styles.headerContainer}>
+            <h1 className={styles.header}>Meet The Development Team</h1>
+            <div className={styles.line}></div>
+        </div>
         <div className={styles.profileContainer}>
             <div className={styles.profileContent}>
                 <img className={styles.profilePicture} src='/images/jason.png' alt='Jason Profile Picture'></img>
-                <h2 className={styles.header}>Jason Khotsombath</h2>
-                <p className={styles.profileText}>Lead Programmer</p>
+
+                <div className={styles.profileText}>
+                    <h2 className={styles.profileName}>Jason Khotsombath</h2>
+                    <p className={styles.profileRole}>Lead Programmer</p>
+                    <a href="https://github.com/DootDaMoop"><img className={styles.socialLinks} src='images/github.png' alt='Github Icon'></img></a>
+                    <a href="https://www.linkedin.com/in/jason-khotsombath/"><img className={styles.socialLinks} src='images/linkedin.png' alt='LinkedIn Icon'></img></a>
+                </div>
+
             </div>
             <div className={styles.profileContent}>
-                <img className={styles.profilePicture} src='/images/ben.png' alt='Lena Profile Picture'></img>
-                <h2 className={styles.header}>Lena Mai</h2>
-                <p className={styles.profileText}>Lead Artist</p>
+                <img className={styles.profilePicture} src='/images/lena.png' alt='Lena Profile Picture'></img>
+
+                <div className={styles.profileText}>
+                    <h2 className={styles.profileName}>Lena Mai</h2>
+                    <p className={styles.profileRole}>Lead Artist</p>
+                    <a href="https://github.com/LenoMai"><img className={styles.socialLinks} src='images/github.png' alt='Github Icon'></img></a>
+                    <a href="https://www.linkedin.com/in/lenamai/"><img className={styles.socialLinks} src='images/linkedin.png' alt='LinkedIn Icon'></img></a>
+                </div>
+
             </div>
             <div className={styles.profileContent}>
-                <img className={styles.profilePicture} src='/images/lena.png' alt='Ben Profile Picture'></img>
-                <h2 className={styles.header}>Ben Tong</h2>
-                <p className={styles.profileText}>UX/UI Design</p>
+                <img className={styles.profilePicture} src='/images/ben.png' alt='Ben Profile Picture'></img>
+
+                <div className={styles.profileText}>
+                    <h2 className={styles.profileName}>Ben Tong</h2>
+                    <p className={styles.profileRole}>UX/UI Design</p>
+                    <a href="https://github.com/benzo1233"><img className={styles.socialLinks} src='images/github.png' alt='Github Icon'></img></a>
+                    <a href="https://www.linkedin.com/in/anh-kiet-tong/"><img className={styles.socialLinks} src='images/linkedin.png' alt='LinkedIn Icon'></img></a>
+                </div>
+
             </div>
         </div>
     </div>

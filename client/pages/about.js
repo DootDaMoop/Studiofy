db46be98c72d5f558b57aba84ad360c8cd39c0df
LenoMai <lenamai4002@gmail.com>
2024-07-11 02:44:22 -0400

    Turned Menu Button into react component and optimized it.

--- client/pages/about.js
+++ client/pages/about.js
@@ -1,67 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import aboutStyles from "../styles/About.module.css"
-import styles from "../styles/apt.module.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faAngleDown, faCircle} from '@fortawesome/free-solid-svg-icons'
+import aboutStyles from "/styles/About.module.css"
+import MenuButton from "../pages/api/menuButton";
 
 function about() {
 
-    const [profile, setProfile] = useState(null);
-    const [toggle, setToggle] = useState(false);
-
-    useEffect(() => {
-        fetch('http://localhost:8080/profile', {
-            credentials: 'include'
-        })
-        .then((response) => {
-            if(!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`)
-            }
-            return response.json();
-        })
-        .then((data) => {
-            setProfile(data);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-    }, []);
-
-    if(!profile) {
-        return <h1>Loading...</h1>;
-    }
-
   return (
     <>
-    
-        <header>
-            <button onClick={() => setToggle(!toggle)} className={styles.button}>
-                <div className={styles.profileimg} style={{ backgroundImage: `url(${profile.images[0].url})` }}></div>
-                <p className={styles.font} >Menu</p>  
-                <i><FontAwesomeIcon icon={faAngleDown} /></i>
-            </button>
-            
-            {toggle && 
-                    <ul className={styles.dropdown}>
-                        <li>
-                            <a className={styles.font}  href='/privacy'>
-                                <p>Privacy </p>     
-                            </a>
-                        </li>
-
-                        <li >
-                            <a className={styles.font}  href='/about'>
-                                <p>About</p>
-                            </a>
-                        </li>
-
-                        <li >
-                            <a className={styles.font}  href='http://localhost:3000/'>
-                                <p>Logout</p>
-                            </a>  
-                        </li>
-                    </ul> }
-        </header>
+        <MenuButton />
 
         <div className={aboutStyles.container}>
 

cf56a230cb78d45fd3efedb91fd134ac0f2903dc
LenoMai <lenamai4002@gmail.com>
2024-08-02 22:45:18 -0400

    Changed Role Names

--- client/pages/about.js
+++ client/pages/about.js
@@ -30,7 +30,7 @@ function about() {
 
                     <div className={aboutStyles.profileText}>
                         <h2 className={aboutStyles.profileName}>Jason Khotsombath</h2>
-                        <p className={aboutStyles.profileRole}>Lead Programmer</p>
+                        <p className={aboutStyles.profileRole}>Project Manager / Back-End Dev</p>
                         <a href="https://github.com/DootDaMoop"><img className={aboutStyles.socialLinks} src='images/github.png' alt='Github Icon'></img></a>
                         <a href="https://www.linkedin.com/in/jason-khotsombath/"><img className={aboutStyles.socialLinks} src='images/linkedin.png' alt='LinkedIn Icon'></img></a>
                     </div>
@@ -42,7 +42,7 @@ function about() {
 
                     <div className={aboutStyles.profileText}>
                         <h2 className={aboutStyles.profileName}>Lena Mai</h2>
-                        <p className={aboutStyles.profileRole}>Front-End Developer</p>
+                        <p className={aboutStyles.profileRole}>Product Manager / Front-End Dev</p>
                         <a href="https://github.com/LenoMai"><img className={aboutStyles.socialLinks} src='images/github.png' alt='Github Icon'></img></a>
                         <a href="https://www.linkedin.com/in/lenamai/"><img className={aboutStyles.socialLinks} src='images/linkedin.png' alt='LinkedIn Icon'></img></a>
                     </div>
@@ -53,7 +53,7 @@ function about() {
 
                     <div className={aboutStyles.profileText}>
                         <h2 className={aboutStyles.profileName}>Ben Tong</h2>
-                        <p className={aboutStyles.profileRole}>Front-End Developer</p>
+                        <p className={aboutStyles.profileRole}>Lead Front-End Dev</p>
                         <a href="https://github.com/benzo1233"><img className={aboutStyles.socialLinks} src='images/github.png' alt='Github Icon'></img></a>
                         <a href="https://www.linkedin.com/in/anh-kiet-tong/"><img className={aboutStyles.socialLinks} src='images/linkedin.png' alt='LinkedIn Icon'></img></a>
                     </div>
@@ -64,7 +64,7 @@ function about() {
 
                     <div className={aboutStyles.profileText}>
                         <h2 className={aboutStyles.profileName}>Angela Yang</h2>
-                        <p className={aboutStyles.profileRole}>Lead UI/UX Designer</p>
+                        <p className={aboutStyles.profileRole}>Lead UI/UX Designer / Artist</p>
                         <a href="https://jinyayuu.carrd.co/"><img className={aboutStyles.socialLinks} src='images/caard.png' alt='Carrd Icon'></img></a>
                         <a href= "mailto: jinya.art@gmail.com"><img className={aboutStyles.socialLinks} src='images/mail.png' alt='Mail Icon'></img></a>
                     </div>
